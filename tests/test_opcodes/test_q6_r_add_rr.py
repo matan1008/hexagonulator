4fcc0ae010513c14c9b7ba7a49dca1cc75caa18d
Matan Perelman <matan1008@gmail.com>
2022-09-27 22:39:20 +0300

    Fix test name

--- tests/test_opcodes/test_q6_r_add_rr.py
+++ tests/test_opcodes/test_q6_r_add_rr.py
@@ -1,4 +1,4 @@
-def test_q6_r_add_ri(hexagon):
+def test_q6_r_add_rr(hexagon):
     hexagon.memory.controllers[0].mem.write(0, 4, b'\x02\xc1\x00\xf3')
     hexagon.registers.general[0] = 3
     hexagon.registers.general[1] = 4

6019b25e4b04d716580dea3a5304e6480d7542e0
Matan Perelman <matan1008@gmail.com>
2022-10-15 23:55:32 +0300

    ReadDCircularIncrement: Refactor circular logic

--- hexgonulator/v67/instructions/abstract/read_d_circular_increment.py
+++ hexgonulator/v67/instructions/abstract/read_d_circular_increment.py
@@ -15,13 +15,8 @@ class ReadDCircularIncrement(Instruction):
         yield
         m = processor.registers.m0 if self.mu == 0 else processor.registers.m1
         offset = self.imm if self.imm is not None else to_signed(m.i << 3, 14)
-        new_pointer = ea + offset
         start_addr = processor.registers.cs0 if self.mu == 0 else processor.registers.cs1
-        end_addr = start_addr + m.length
-        if new_pointer >= end_addr:
-            new_pointer -= m.length
-        elif new_pointer < start_addr:
-            new_pointer += m.length
+        new_pointer = ((ea + offset - start_addr) % m.length) + start_addr
         data = processor.mem_get(ea, 8)
         yield
         processor.registers.general[self.x] = new_pointer

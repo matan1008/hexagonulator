07f7e1d4b043b504bf03803123dff00ce7af0db9
Matan Perelman <matan1008@gmail.com>
2022-10-25 22:10:19 +0300

    ConditionalReadHRegImmNew: Fix test

--- tests/test_opcodes/test_conditional_read_h_reg_imm_new.py
+++ tests/test_opcodes/test_conditional_read_h_reg_imm_new.py
@@ -13,7 +13,7 @@ def test_conditional_read_h_reg_imm_new(hexagon):
 def test_conditional_read_h_reg_imm_new_false(hexagon):
     hexagon.memory.controllers[0].mem.write(0, 4, b'\x21\xc0\x40\x43')
     add_memory(hexagon, b'\x00\xf0\x00\x00', 0x40000020)
-    hexagon.registers.general[0] = 0x4000001f
+    hexagon.registers.general[0] = 0x4000001e
     hexagon.registers.predicate[0] = 1
     hexagon.xunits = HookedXunits(hexagon, lambda: set_predicate(hexagon, 0))
     hexagon.cycle()
